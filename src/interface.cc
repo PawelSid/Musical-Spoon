802aa4e10f47c9a59502c9e360659c8021436f9a
Jakub Korsak <korsakuba@gmail.com>
2019-06-13 19:58:59 +0200

    extract nums z stringów

--- src/interface.cc
+++ src/interface.cc
@@ -6,6 +6,12 @@ int Interface::extract_int_from_string(std::string str){
     return out;
 }
 
+double Interface::extract_double_from_string(std::string str){
+    double out;
+    std::istringstream ( str ) >> out;
+    return out;
+}
+
 void Interface::print(std::string s){
 
 }
--- src/interface.h
+++ src/interface.h
@@ -5,5 +5,6 @@ class Interface
 {
     public:
         int extract_int_from_string(std::string s);
+        double extract_double_from_string(std::string s);
         void print(std::string s);
 };
